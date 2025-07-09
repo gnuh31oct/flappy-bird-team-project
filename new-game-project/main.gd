9a4cb46416d4d2fad1eff959025c08cb57f416c6
yuukimoona17 <163424867+yuukimoona17@users.noreply.github.com>
2025-07-09 22:17:10 +0700

    cac

--- new-game-project/main.gd
+++ new-game-project/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var pipe_scene = preload("res://pipe.tscn")
 func _on_timer_timeout():
-    var new_pipe = pipe_scene.instantiate()
-    var y_pos = randf_range(200, 450)
-    new_pipe.position = Vector2(get_viewport_rect().size.x + 50, y_pos)
-    add_child(new_pipe)
+	var new_pipe = pipe_scene.instantiate()
+	var y_pos = randf_range(200, 450)
+	new_pipe.position = Vector2(get_viewport_rect().size.x + 50, y_pos)
+	add_child(new_pipe)
