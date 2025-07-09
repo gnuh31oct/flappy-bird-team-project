ef5c674f1b6d22d87a6503dc4833daafc1292490
xuanhung <160983335+gnuh31oct@users.noreply.github.com>
2025-07-09 22:03:06 +0700

    Update pipe.gd

--- new-game-project/pipe.gd
+++ new-game-project/pipe.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 var speed = 150
 
+
+func _ready():
+    
+    
+    $VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
+
 func _process(delta):
-	position.x -= speed * delta
-	if position.x < -500:
-		queue_free()
+    
+    position.x -= speed * delta
