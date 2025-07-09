4cfe49477de35f9164028be8a20c81269a3e41a6
yuukimoona17 <163424867+yuukimoona17@users.noreply.github.com>
2025-07-09 23:04:43 +0700

    xuanhungancut

--- new-game-project/pipe.gd
+++ new-game-project/pipe.gd
@@ -2,7 +2,10 @@ extends Node2D
 signal score
 var speed = 150
 func _process(delta):
-    position.x -= speed * delta
-    if position.x < -100: queue_free()
+	position.x -= speed * delta
+	if position.x < -100: queue_free()
 func _on_score_area_body_entered(body):
-    emit_signal("score")
+	emit_signal("score")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
