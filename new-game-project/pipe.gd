f8971ba3b28348d04deb7e4b320a109d0aaaed3b
xuanhung <160983335+gnuh31oct@users.noreply.github.com>
2025-07-09 21:53:07 +0700

    Update pipe.gd

--- new-game-project/pipe.gd
+++ new-game-project/pipe.gd
@@ -1,5 +1,10 @@
 extends Node2D
+
 var speed = 150
+
 func _process(delta):
     position.x -= speed * delta
-    if position.x < -100: queue_free()
+
+  
+    if position.x < -200: 
+        queue_free()
