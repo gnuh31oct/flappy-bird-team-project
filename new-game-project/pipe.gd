12dce4d6206175db7fe9c1fe5814dee3efbb95f5
yuukimoona17 <163424867+yuukimoona17@users.noreply.github.com>
2025-07-09 23:16:21 +0700

    deo cho

--- new-game-project/pipe.gd
+++ new-game-project/pipe.gd
@@ -5,14 +5,14 @@ signal score
 var speed = 150
 
 func _ready():
-    # Kết nối tín hiệu khi ra khỏi màn hình để tự hủy
-    $VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
+	# Kết nối tín hiệu khi ra khỏi màn hình để tự hủy
+	$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
 
 func _process(delta):
-    position.x -= speed * delta
+	position.x -= speed * delta
 
 # Hàm này sẽ được kết nối với Area2D
 func _on_score_area_body_entered(body):
-    # Để tránh tính điểm 2 lần, tắt va chạm của vùng tính điểm ngay khi nó được kích hoạt
-    $ScoreArea/CollisionShape2D.disabled = true
-    emit_signal("score")
+	# Để tránh tính điểm 2 lần, tắt va chạm của vùng tính điểm ngay khi nó được kích hoạt
+	$ScoreArea/CollisionShape2D.disabled = true
+	emit_signal("score")
--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -6,13 +6,13 @@ const JUMP_VELOCITY = -350.0
 var gravity = 980.0
 
 func _physics_process(delta):
-    if not is_on_floor():
-        velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-    if Input.is_action_just_pressed("ui_accept"):
-        velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_VELOCITY
 
-    var is_colliding = move_and_slide()
-    if is_colliding:
-        emit_signal("hit")
-        set_physics_process(false)
+	var is_colliding = move_and_slide()
+	if is_colliding:
+		emit_signal("hit")
+		set_physics_process(false)
