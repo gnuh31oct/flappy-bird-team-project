37c19902557e0312426b39551a4aca608f796c1f
xuanhung <160983335+gnuh31oct@users.noreply.github.com>
2025-07-09 21:29:38 +0700

    Update player.gd

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+const JUMP_VELOCITY = -350.0
+var gravity = 980.0
+
+func _physics_process(delta):
+    if not is_on_floor():
+        velocity.y += gravity * delta
+
+    if Input.is_action_just_pressed("ui_accept"):
+        velocity.y = JUMP_VELOCITY
+
+    move_and_slide()
