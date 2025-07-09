1b9ffb18a6213d982520543f13ac2ed52e5092b9
xuanhung <160983335+gnuh31oct@users.noreply.github.com>
2025-07-09 23:06:06 +0700

    sad

--- new-game-project/pipe.gd
+++ new-game-project/pipe.gd
@@ -1,11 +1,18 @@
 extends Node2D
-signal score
-var speed = 150
-func _process(delta):
-	position.x -= speed * delta
-	if position.x < -100: queue_free()
-func _on_score_area_body_entered(body):
-	emit_signal("score")
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+signal score
+
+var speed = 150
+
+func _ready():
+ 
+    $VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
+
+func _process(delta):
+    position.x -= speed * delta
+
+
+func _on_score_area_body_entered(body):
+    
+    $ScoreArea/CollisionShape2D.disabled = true
+    emit_signal("score")
