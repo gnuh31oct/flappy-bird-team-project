61a28f90e7a73893e6e36bb369c7f2d01908dde9
yuukimoona17 <163424867+yuukimoona17@users.noreply.github.com>
2025-07-09 22:10:17 +0700

    Dung xong scene Main

--- new-game-project/pipe.gd
+++ new-game-project/pipe.gd
@@ -4,10 +4,10 @@ var speed = 150
 
 
 func _ready():
-    
-    
-    $VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
+	
+	
+	$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
 
 func _process(delta):
-    
-    position.x -= speed * delta
+	
+	position.x -= speed * delta
